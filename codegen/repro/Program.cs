0c2baf0142ce311a84e5cbf6a850aee35547d6e6
odalet <odalet@gmail.com>
2020-02-02 17:51:09 +0100

    Embedded Mono.Cecil for ease of debugging

--- codegen/repro/Program.cs
+++ codegen/repro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
@@ -18,10 +19,31 @@ namespace repro
         {
             const string assemblyAndMainModuleName = "app";
 
+#if NETCOREAPP
+            var corlibReference = new AssemblyNameReference("System.Runtime", new Version(4, 2, 1, 0))
+            {
+                PublicKeyToken = new byte[] { 0xb0, 0x3f, 0x5f, 0x7f, 0x11, 0xd5, 0x0a, 0x3a }
+            };
+#else
+            var corlibReference = new AssemblyNameReference("mscorlib", new Version(4, 0, 0, 0))
+            {
+                PublicKeyToken = new byte[] { 0xb7, 0x7a, 0x5c, 0x56, 0x19, 0x34, 0xe0, 0x89 }
+            };
+#endif
+            //var resolver = new DefaultAssemblyResolver();
+            //var corlib = resolver.Resolve(corlibReference);
+            //var privlibReference = corlib.MainModule.AssemblyReferences.Single(a => a.Name == "System.Private.CoreLib");
+            //var privlib = resolver.Resolve(privlibReference);
+
+            //var stringType = privlib.MainModule.Types.Single(t => t.Namespace == "System" && t.Name == "String");
+            //var consoleType = privlib.MainModule.Types.Single(t => t.Namespace == "System" && t.Name == "Console");
+            //var writeLineMethod = consoleType.Methods.Single(m => m.Name == "WriteLine" && m.Parameters.Count == 1 && m.Parameters[0].ParameterType == stringType);
+
             var assemblyNameDefinition = new AssemblyNameDefinition(assemblyAndMainModuleName, new Version(1, 0, 0, 0));
             using (var assemblyDefinition = AssemblyDefinition.CreateAssembly(assemblyNameDefinition, assemblyAndMainModuleName, ModuleKind.Console))
             {
                 var mainModule = assemblyDefinition.MainModule;
+                mainModule.AssemblyReferences.Add(corlibReference);
                 mainModule.RuntimeVersion = "v4.0.30319";
 
                 var type = new TypeDefinition("app", "Program", TypeAttributes.NotPublic | TypeAttributes.Sealed, mainModule.TypeSystem.Object)
@@ -31,6 +53,7 @@ namespace repro
 
                 var main = new MethodDefinition("Main", MethodAttributes.Private | MethodAttributes.Static, mainModule.TypeSystem.Void);
 
+                //var writeLine = mainModule.ImportReference(writeLineMethod);
                 var writeLine = mainModule.ImportReference(typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
 
                 var il = main.Body.GetILProcessor();
@@ -43,9 +66,25 @@ namespace repro
                 type.Methods.Add(main);
                 mainModule.Types.Add(type);
 
+#if NETCOREAPP
+                var targetFrameworkAttribute = new CustomAttribute(mainModule.ImportReference(
+                    typeof(System.Runtime.Versioning.TargetFrameworkAttribute).GetConstructor(new[] { typeof(string) })));
+                targetFrameworkAttribute.ConstructorArguments.Add(
+                    new CustomAttributeArgument(mainModule.TypeSystem.String, ".NETCoreApp,Version=v3.0"));
+                targetFrameworkAttribute.Properties.Add(new CustomAttributeNamedArgument("FrameworkDisplayName",
+                    new CustomAttributeArgument(mainModule.TypeSystem.String, "")));
+#else
+                var targetFrameworkAttribute = new CustomAttribute(mainModule.ImportReference(
+                    typeof(System.Runtime.Versioning.TargetFrameworkAttribute).GetConstructor(new[] { typeof(string) })));
+                targetFrameworkAttribute.ConstructorArguments.Add(
+                    new CustomAttributeArgument(mainModule.TypeSystem.String, ".NETFramework,Version=v4.7.2"));
+                targetFrameworkAttribute.Properties.Add(new CustomAttributeNamedArgument("FrameworkDisplayName",
+                    new CustomAttributeArgument(mainModule.TypeSystem.String, ".NET Framework 4.7.2")));
+#endif
                 mainModule.EntryPoint = main;
-
                 assemblyDefinition.EntryPoint = main;
+
+                assemblyDefinition.CustomAttributes.Add(targetFrameworkAttribute);
                 assemblyDefinition.Write(filename);
             }
         }
